b257831ce5ab6396e4193ddc7059b7ff1130e2ae
kefeng <kefeng@baijiayun.com>
2020-02-27 22:51:05 +0800

    init socket.io

--- rtc-front/src/views/Room.vue
+++ rtc-front/src/views/Room.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-02-22 22:21:25
- * @LastEditTime: 2020-02-24 22:09:55
+ * @LastEditTime: 2020-02-27 22:49:27
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /rtc-meeting/rtc-front/src/views/Room.vue
@@ -14,10 +14,18 @@
     <div class="remote-video">
       <video class="video" src></video>
     </div>
+
+    <div class="btn-wrap">
+      <v-btn>start</v-btn>
+      <v-btn @click="sendMessage()">call</v-btn>
+      <v-btn>hang up</v-btn>
+    </div>
   </div>
 </template>
 
 <script>
+import io from '../assets/js/socket.io'
+
 export default {
   data() {
     return {
@@ -26,8 +34,16 @@ export default {
         audio: false
       },
       localStream: null,
-      localVideo: null
+      localVideo: null,
+      socket: null
     }
+  },
+  created() {
+    let socket = io('http://127.0.0.1:3001')
+    this.socket = socket
+    socket.on('connect', () => {
+      console.log('socket is openning.')
+    })
   },
   mounted() {
     this.localVideo = document.getElementById('local-video')
@@ -43,6 +59,10 @@ export default {
     },
     handleLocalMediaStreamError(err) {
       console.log('get local video stream failed.', err)
+    },
+    sendMessage() {
+      console.log('123')
+      this.socket.emit('message', { content: 'hello server.' })
     }
   }
 }
@@ -51,6 +71,6 @@ export default {
 <style lang="scss" scoped>
 .video {
   width: 400px;
-  filter: blur(4px) invert(1) opacity(0.5);
+  // filter: blur(4px) invert(1) opacity(0.5);
 }
 </style>
---
+++ server/index.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: your name
+ * @Date: 2020-02-27 21:52:57
+ * @LastEditTime: 2020-02-27 22:47:30
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /rtc-meeting/server/index.js
+ */
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+io.on('connection', socket => {
+  console.log('user connected.')
+
+  socket.on('message', data => {
+    console.log(data)
+  })
+
+  socket.broadcast.emit('open')
+})
+
+http.listen(3001, () => {
+  console.log("app is running on port 3000.")
+})
