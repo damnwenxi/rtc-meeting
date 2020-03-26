53dcb9dc36cf35e45ccdfef28f98a2cbed446859
solotor <kefeng1026@gmail.com>
2020-03-26 23:39:38 +0800

    完善前端登录逻辑

--- rtc-front/src/components/NavBar.vue
+++ rtc-front/src/components/NavBar.vue
@@ -1,36 +1,75 @@
 <!--
  * @Author: kefeng
  * @Date: 2020-02-22 21:43:49
- * @LastEditTime: 2020-02-23 23:46:35
+ * @LastEditTime: 2020-03-26 23:38:07
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /rtc-meeting/rtc-front/src/components/NavBar.vue
  -->
 <template>
   <v-app-bar absolute color="white" hide-on-scroll scroll-target="#container">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-    <v-toolbar-title>RTC meeting</v-toolbar-title>
+    <v-icon @click="goHome()">mdi-domain</v-icon>
+    <v-toolbar-title class="title">RTC meeting</v-toolbar-title>
 
     <v-spacer></v-spacer>
 
-    <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
+    <v-btn @click="goLogin()" v-if="!isLogin" text>
+      <v-icon dark>mdi-account-circle</v-icon>登录
     </v-btn>
 
-    <v-btn icon>
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
-
-    <v-btn icon>
-      <v-icon>mdi-dots-vertical</v-icon>
-    </v-btn>
+    <v-menu
+      v-else
+      v-model="userMenu"
+      :close-on-click="true"
+      :close-on-content-click="true"
+      :offset-y="true"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn text v-on="on">
+          <v-avatar size="36px">
+            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+          </v-avatar>
+          <strong v-html="'kefeng'"></strong>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item @click="logout()">
+          退出登录
+          <v-icon>mdi-export</v-icon>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      userMenu: false,
+      isLogin: false
+    }
+  },
+  methods: {
+    logout() {
+      console.log('logout')
+    },
+    goLogin() {
+      this.$router.push({
+        name: 'Login'
+      })
+    },
+    goHome() {
+      this.$router.push({
+        name: 'Home'
+      })
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
+.title {
+  margin-left: 10px;
+}
 </style>
